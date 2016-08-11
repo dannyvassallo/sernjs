98fa99e18ca3ea861fb26299a465da1229cccc2f
dannyvassallo <danielvassallo87@gmail.com>
2016-08-11 10:41:09 -0400

    foreman listens on 5000

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, './client/public');
 var APP_DIR = path.resolve(__dirname, './client/app');
-
+var PORT_NUM = 5000
 // SET PORT FOR HEROKU DEPLOYMENT
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || PORT_NUM));
 
 app.use(express.static(path.join(__dirname, './client/public')));
 
@@ -15,5 +15,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(app.get('port'), function () {
-  console.log('App is listening on port 3000! Visit localhost:3000 in your browser.');
+  console.log('App is listening on port '+PORT_NUM+'! Visit localhost:'+PORT_NUM+' in your browser.');
 });
