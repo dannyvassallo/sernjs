821bbc317cfbe6b83057110d49a649b1e9413ad4
dannyvassallo <danielvassallo87@gmail.com>
2016-08-17 10:38:43 -0400

    moved find counter to its own function

--- routes/api/counter.js
+++ routes/api/counter.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var models  = require('../../server/models');
 
-router.get('/', function(req, res){
+function findOrCreateNewCounter(){
   models.Counter.findById(1).then(function(counter){
     if(counter == null){
       var counter = models.Counter.create().then(function(newCounter) {
@@ -12,6 +12,10 @@ router.get('/', function(req, res){
       console.log(counter);
     }
   });
+}
+
+router.get('/', function(req, res){
+  findOrCreateNewCounter();
   res.json({ increment: 1 });
 });
 
