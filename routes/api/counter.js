15f28acc1b6f4aeffae2e43cc8e120517bd2f57c
dannyvassallo <danielvassallo87@gmail.com>
2016-08-17 10:33:15 -0400

    api call on click now creates a new counter if one doesnt exist in db

--- routes/api/counter.js
+++ routes/api/counter.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var models  = require('../../server/models');
 
 router.get('/', function(req, res){
+  models.Counter.findById(1).then(function(counter){
+    if(counter == null){
+      var counter = models.Counter.create().then(function(newCounter) {
+        console.log(newCounter);
+      });
+    } else {
+      console.log(counter);
+    }
+  });
   res.json({ increment: 1 });
 });
 
