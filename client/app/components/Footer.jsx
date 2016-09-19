56da8a1f539af45adff42d0a2c40181d48995742
dannyvassallo <danielvassallo87@gmail.com>
2016-09-19 12:57:11 -0400

    added fontawesome and stamp in footer

--- client/app/components/Footer.jsx
+++ client/app/components/Footer.jsx
@@ -47,8 +47,15 @@ var Footer = React.createClass({
               }
             </ul>
           </div>
-          <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">2</div>
-          <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">3</div>
+          <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <h2>About</h2>
+            <p>sern.js is a Full-Stack Javascript for an easy starting point with SequelizeJS, ExpressJS, PassportJS, ReactJS and NodeJS based applications.</p>
+            <p>It is designed to give you a quick and organized way to start developing SERN based web apps.</p>
+          </div>
+          <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <h2>Info</h2>
+            <p>Made with <i className="fa fa-heart" aria-hidden="true"></i> at Trendsetter Media & Marketing by <a href="https://github.com/dannyvassallo" target="_blank">@dannyvassallo</a> <i className="fa fa-github" aria-hidden="true"></i></p>
+          </div>
         </div>
       </div>
     );
